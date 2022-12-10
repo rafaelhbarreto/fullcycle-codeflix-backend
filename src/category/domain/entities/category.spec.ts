2c6e860b0f1655c2f1b3fc3525914326f9b794d1
Rafael Barreto <rafaelhbarreto@gmail.com>
2022-12-10 13:55:02 -0300

    Create the category entity with tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,10 +1,36 @@
 import { Category } from "./category";
 
-describe('Category tests', () => {
+describe('Category unit tests', () => {
   
-  test('test category', () => {
-    const category = new Category('Movie'); 
-    expect(category.name).toBe('Movie'); 
+  test('test category constructor', () => {
+    const propsCategory = {
+      name: 'Some name',
+      description: 'Some description',
+      is_active: true,
+      date: new Date(),
+    }
+
+    const category = new Category(propsCategory);
+    
+    expect(category.name).toBe(propsCategory.name);
+    expect(category.description).toBe(propsCategory.description);
+    expect(category.is_active).toBe(propsCategory.is_active);
+    expect(category.date).toBe(propsCategory.date);
   }); 
+
+  test('test category without optional props', () => {
+
+    const propsCategory = {
+      name: 'some name'
+    }
+
+    const category = new Category(propsCategory); 
+
+    expect(category.name).toBe(propsCategory.name); 
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeTruthy();
+    expect(category.date).not.toBeNull();
+    expect(category.date).toBeInstanceOf(Date);
+  });
 
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,43 @@
+interface CategoryProperties {
+  name: string,
+  description?: string,
+  is_active?: boolean,
+  date?: Date
+}
+
 export class Category 
 {
-  constructor(public name: string = '') {
-    this.name = name; 
+  constructor(public readonly props: CategoryProperties) {
+    this.description = props.description ?? null; 
+    this.is_active = props.is_active ?? true; 
+    this.date = props.date ?? new Date(); 
+  }
+
+  get name(): string {
+    return this.props.name; 
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  private set description(str: string) {
+    this.props.description = str; 
+  }
+
+  get is_active(): boolean {
+    return this.props.is_active; 
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value;
+  }
+
+  get date(): Date {
+    return this.props.date;
+  }
+
+  private set date(date: Date) {
+    this.props.date = date; 
   }
 }
